c02916c5835b5dc032f1be62606ec3e5c908d2b2
Dieisson03032000 <57776604+Dieisson03032000@users.noreply.github.com>
2021-10-05 19:02:49 -0300

    Add files via upload

---
+++ 4-C.sql
@@ -0,0 +1,12 @@
+SET SEARCH_PATH TO TRABALHO_G1;
+
+CREATE OR REPLACE FUNCTION decre_ingresso () 
+RETURNS trigger AS $$
+	BEGIN
+		update sessao set ingressos_disponiveis = ingressos_disponiveis - 1
+		
+CREATE TRIGGER decre_ingre_disponivel AFTER INSERT ON reserva
+	FOR EACH ROW
+		EXECUTE FUNCTION decre_ingresso();		
+		
+		
