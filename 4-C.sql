c550f60f60f2231eeefc05b2f1b1331f2327b362
Dieisson03032000 <57776604+Dieisson03032000@users.noreply.github.com>
2021-10-07 18:42:33 -0300

    Update 4-C.sql

--- 4-C.sql
+++ 4-C.sql
@@ -3,10 +3,11 @@ SET SEARCH_PATH TO TRABALHO_G1;
 CREATE OR REPLACE FUNCTION decre_ingresso () 
 RETURNS trigger AS $$
 	BEGIN
-		update sessao set ingressos_disponiveis = ingressos_disponiveis - 1
-		
+		update sessao set ingressos_disponiveis = ingressos_disponiveis - 1;
+	END;
+	
+$$ LANGUAGE plpgsql	
+
 CREATE TRIGGER decre_ingre_disponivel AFTER INSERT ON reserva
 	FOR EACH ROW
-		EXECUTE FUNCTION decre_ingresso();		
-		
-		
+		EXECUTE FUNCTION decre_ingresso();	
