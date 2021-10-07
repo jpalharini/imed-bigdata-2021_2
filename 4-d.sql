999d12618ac0c1f775d89f8c7c5c0b46c49f7f5e
Dieisson03032000 <57776604+Dieisson03032000@users.noreply.github.com>
2021-10-07 18:42:56 -0300

    Update 4-d.sql

--- 4-d.sql
+++ 4-d.sql
@@ -3,8 +3,11 @@ SET SEARCH_PATH TO TRABALHO_G1;
 CREATE OR REPLACE FUNCTION incre_ingresso () 
 RETURNS trigger AS $$
 	BEGIN
-		update sessao set ingressos_disponiveis = ingressos_disponiveis + 1
-		
+		update sessao set ingressos_disponiveis = ingressos_disponiveis + 1;
+	END;
+	
+$$ LANGUAGE plpgsql	
+
 CREATE TRIGGER incre_ingre_disponivel AFTER DELETE ON reserva
 	FOR EACH ROW
-		EXECUTE FUNCTION incre_ingresso();	
+		EXECUTE FUNCTION incre_ingresso();		
